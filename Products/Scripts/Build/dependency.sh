cb3549fd1c311991af7667bbe3a37c0bed95808b
cavan <403029552@qq.com>
2025-05-28 14:52:19 +0800

    [FEAT]: update dependency.sh version to 2.8.111

--- Products/Scripts/Build/dependency.sh
+++ Products/Scripts/Build/dependency.sh
@@ -6,7 +6,7 @@
 # UIBaseViews
 Artifactory_iOS_URL="https://artifactory.agoralab.co/artifactory/AD_repo/aPaaS/iOS"
 
-Version="2.8.110"
+Version="2.8.111"
 Branch="release_${Version}"
 
 AgoraWidget_URL="${Artifactory_iOS_URL}/AgoraWidget/${Branch}/dev/AgoraWidget_${Version}.zip"
