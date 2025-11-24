d08ce92ad567b12b93e23336f9f85752e6da7328
vitas <lcztt@users.noreply.github.com>
2025-11-24 15:25:57 +0800

    [FEAT]: update dependency.sh version to 2.8.122-hello

--- Products/Scripts/Build/dependency.sh
+++ Products/Scripts/Build/dependency.sh
@@ -6,7 +6,7 @@
 # UIBaseViews
 Artifactory_iOS_URL="https://artifactory.agoralab.co/artifactory/AD_repo/aPaaS/iOS"
 
-Version="2.8.122"
+Version="2.8.122-hello"
 Branch="release_${Version}"
 
 AgoraWidget_URL="${Artifactory_iOS_URL}/AgoraWidget/${Branch}/dev/AgoraWidget_${Version}.zip"
