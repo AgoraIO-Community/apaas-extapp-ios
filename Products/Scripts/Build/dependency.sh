d4e23bbbdb776e37bf9f7e8b4e17f25b747e4c1d
ZH <zhonghuan@agora.io>
2026-07-23 14:12:54 +0800

    [FEAT]: [APAAS-16218] update dependency.sh version to 2.8.128-hello

--- Products/Scripts/Build/dependency.sh
+++ Products/Scripts/Build/dependency.sh
@@ -6,7 +6,7 @@
 # UIBaseViews
 Artifactory_iOS_URL="https://artifactory.agoralab.co/artifactory/AD_repo/aPaaS/iOS"
 
-Version="2.8.127-hello"
+Version="2.8.128-hello"
 Branch="release_${Version}"
 
 AgoraWidget_URL="${Artifactory_iOS_URL}/AgoraWidget/${Branch}/dev/AgoraWidget_${Version}.zip"
